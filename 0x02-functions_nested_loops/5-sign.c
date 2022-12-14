64bf39dd123fffcc557b9b907ed99335a3a229e3
Miriam-Ngugi <waitherangugi34@gmail.com>
2022-12-14 14:57:38 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - Returns 0 when n is greter than 0, 1 otherwise
+ * @n: number to be checked
+ *
+ * Return: 0 or 1
+ */
+int print_sign(int n)
+{
+	int test;
+
+	if (n > 0)
+	{
+		test = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		test = 0;
+		_putchar('0');
+	}
+	else
+	{
+		test = -1;
+		_putchar('-');
+	}
+	return (test);
+}
