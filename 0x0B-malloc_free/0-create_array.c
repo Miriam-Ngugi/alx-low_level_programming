c775192da9e022dc6074b4fa8f2c5a74dbb9fa0f
Miriam-Ngugi <waitherangugi34@gmail.com>
2023-01-12 00:53:10 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,7 +4,7 @@
 /**
  * create_array - Creates an array of chars and initializes it with a
  * specific char
- * @size: size of the array
+ * @size: size of the array.
  * @c: the character to initialize the array with
  *
  * Return: a pointer to the array or NULL if it fails
