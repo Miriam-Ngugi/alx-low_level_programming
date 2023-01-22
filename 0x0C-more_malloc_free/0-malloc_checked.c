32701c08588908fc6c7078a6f66e937f8b1f1026
Miriam-Ngugi <waitherangugi34@gmail.com>
2023-01-22 14:59:58 -0800

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,12 +10,10 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+	unsigned int *i;
 
-	p = malloc(b);
-
-	if (p == NULL)
+	i = malloc(b);
+	if (i == NULL)
 		exit(98);
-
-	return (p);
+	return (i);
 }
