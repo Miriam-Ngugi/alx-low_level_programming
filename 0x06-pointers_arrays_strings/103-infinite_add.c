8f4cf559c5a859599fde00f303f2c08062a32b10
Miriam-Ngugi <waitherangugi34@gmail.com>
2023-01-26 13:01:04 +0000

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,6 @@
-#include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * infinite_add - adds two numbers
  * @n1: number one.
@@ -11,44 +12,39 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
+	int len = (len1 > len2) ? len1 : len2;
+	int carry = 0;
+	int sum;
 
-	while (*(n1 + c1) != '\0')
-		c1++;
-	while (*(n2 + c2) != '\0')
-		c2++;
-	if (c1 >= c2)
-		bg = c1;
-	else
-		bg = c2;
-	if (size_r <= bg + 1)
-		return (0);
-	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-	while (bg >= 0)
+	if (size_r <= len + 1)
 	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
-		else
-			add = 0;
-		if (op > 0)
-		*(r + bg) = (op % 10) + 48;
-		else
-			*(r + bg) = '0';
-		if (c1 > 0)
-			c1--, dr1 = *(n1 + c1) - 48;
-		else
-			dr1 = 0;
-		if (c2 > 0)
-			c2--, dr2 = *(n2 + c2) - 48;
-		else
-			dr2 = 0;
-		bg--, size_r--;
+		return (0);
 	}
-	if (*(r) == '0')
-		return (r + 1);
+
+	r[len + 1] = '\0';
+	while (len1 > 0 || len2 > 0)
+	{
+		sum = carry;
+		if (len1 > 0)
+		{
+			sum += n1[--len1] - '0';
+		}
+		if (len2 > 0)
+		{
+			sum += n2[--len2] - '0';
+		}
+		r[--len] = sum % 10 + '0';
+		carry = sum / 10;
+	}
+	if (carry)
+	{
+		r[--len] = carry + '0';
+	}
 	else
-		return (r);
+	{
+		++r;
+	}
+	return (r);
 }
